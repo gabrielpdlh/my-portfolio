f83dbe5d59fba55c752237bd566f3ce7e44578d9
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2024-09-17 20:43:25 -0300

    feat: add hero section

---
+++ components/ui/MagicButton.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
+
+const MagicButton = ({
+  title,
+  icon,
+  position,
+  handleClick,
+  otherClasses,
+}: {
+  title: string;
+  icon: React.ReactNode;
+  position: string;
+  handleClick?: () => void;
+  otherClasses?: string;
+}) => {
+  return (
+    <button className="relative inline-flex h-12 overflow-hidden w-full rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10">
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#bff7d5_0%,#86efac_50%,#7daf93_100%)]" />
+      <span
+        className={`inline-flex h-full w-full gap-2 cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl ${otherClasses}`}
+      >
+        {position === "left" && icon}
+        {title}
+        {position === "right" && icon}
+      </span>
+    </button>
+  );
+};
+
+export default MagicButton;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -46,6 +46,7 @@ const config = {
           "100": "#E4ECFF",
         },
         purple: "#CBACF9",
+        green: "#86efac",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
