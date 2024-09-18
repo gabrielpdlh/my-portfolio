34840465f06250a3bbff1d7027844a8e991006ba
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2024-09-18 01:04:17 -0300

    refactor: ensure Spolight is correctly

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Spotlight } from "./ui/Spolight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import MagicButton from "./ui/MagicButton";
-import { FaLinkedin, FaGithub  } from "react-icons/fa";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -14,17 +14,16 @@ const Hero = () => {
         />
         <Spotlight
           className="-top-10 left-full h-[80vh] w-[50vw]"
-          fill="green"
+          fill="white"
         />
-        <Spotlight className="-top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+        <Spotlight className="-top-28 left-80 h-[80vh] w-[50vw]" fill="green" />
       </div>
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.3] flex items-center justify-center absolute top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-          </h2>
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80"></h2>
 
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
@@ -34,21 +33,14 @@ const Hero = () => {
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi,I&apos;m Gabriel, a Web Developer
           </p>
-
-          <div>
-            <a className="flex flex-col gap-2 md:flex-row" href="#about">
-              <MagicButton
-                title="Linkedin"
-                icon={<FaLinkedin />}
-                position="left"
-              />
-              <MagicButton
-                title="Github"
-                icon={<FaGithub  />}
-                position="left"
-              />
-            </a>
-          </div>
+          <a className="flex flex-col gap-2 md:flex-row w-full  justify-center" href="#about">
+            <MagicButton
+              title="Linkedin"
+              icon={<FaLinkedin />}
+              position="left"
+            />
+            <MagicButton title="Github" icon={<FaGithub />} position="left" />
+          </a>
         </div>
       </div>
     </div>
